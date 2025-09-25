a3ff5f27b3a01a3bc2eab8466385be285855ac47
David-stn1 <dgutierrez32@ucol.mx>
2025-09-24 22:22:42 -0600

    First commit

--- app/src/main/java/com/example/listado_alumnos/MainActivity.kt
+++ app/src/main/java/com/example/listado_alumnos/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.listado_alumnos
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,21 +9,52 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.listado_alumnos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
 
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerPersonas)
         recyclerview.layoutManager = LinearLayoutManager(this)
         val data = ArrayList<Alumno>()
 
-        for (i in 1 .. 50) {
+        for (i in 1 .. 10) {
             data.add(Alumno("David Antonio Gutierrez", "20202860", "dgutierrez32@ucol.mx", "https://i.pinimg.com/236x/e0/b8/3e/e0b83e84afe193922892917ddea28109.jpg"))
         }
 
         val adapter = AlumnoAdapter(this,data)
         recyclerview.adapter = adapter
+
+        val parExtra = intent.extras
+        val msje = parExtra?.getString("mensaje")
+        val nombre = parExtra?.getString("nombre")
+        val cuenta = parExtra?.getString("cuenta")
+        val correo = parExtra?.getString("correo")
+        val imagen = parExtra?.getString("imagen")
+
+        if (msje == "nuevo") {
+            val insertIndex: Int = data.count()
+            data.add(insertIndex,
+                Alumno(
+                    "${nombre}",
+                    "${cuenta}",
+                    "${correo}",
+                    "${imagen}"
+                )
+            )
+            adapter.notifyItemInserted(insertIndex)
+        }
+
+        binding.faButton.setOnClickListener {
+            val intento1 = Intent(this, MainActivityNuevo::class.java)
+            startActivity(intento1)
+        }
     }
 }
